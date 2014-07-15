c3f67b30b09d71e3ed6643aceb5f1a6d1bedd01a
Julius S <mail@julius-seltenheim.com>
2014-07-16 00:30:12 +0200

    close #7, money adding logic is now in backbone, negative value get red

--- app/assets/js/app/mate.js
+++ app/assets/js/app/mate.js
@@ -1,30 +1,33 @@
-define(
-		[ 'jquery', 'jquerymeta', 'jquerytable', 'app/models/junky',
-				'app/collections/junkies', 'app/views/junkies/row',
-				"app/views/newUser" ], function($, meta, table, Junky, Junkies,
-				JunkyRowView, newUserView) {
-			window.MateDatabase = {};
-			MateDatabase.junkies = new Junkies(junkiesJson);
+define([ 'jquery', 'jquerymeta', 'jquerytable', 'app/models/junky',
+		'app/collections/junkies', 'app/views/junkies/row',
+		"app/views/newUser", "app/views/addMoney" ], function($, meta, table,
+		Junky, Junkies, JunkyRowView, newUserView, addMoneyView) {
+	window.MateDatabase = {};
+	MateDatabase.junkies = new Junkies(junkiesJson);
 
-			MateDatabase.junkies.each(function(model) {
-				new JunkyRowView({
-					el : $("#junky-container"),
-					junky : model
-				});
-			});
-
-			new newUserView({
-				el : $("#newUser-container")
-			});
-
-			$("#junkyTable").tablesorter({
-				sortList : [ [ 3, 1 ] ],
-				headers : {
-					3 : {
-						sorter : 'digit'
-					}
-				}
-			});
-
-			return MateDatabase;
+	MateDatabase.junkies.each(function(model) {
+		new JunkyRowView({
+			el : $("#junky-container"),
+			junky : model
 		});
+	});
+
+	new newUserView({
+		el : $("#newUser-container")
+	});
+
+	new addMoneyView({
+		el : $("#addMoney-container")
+	});
+
+	$("#junkyTable").tablesorter({
+		sortList : [ [ 3, 1 ] ],
+		headers : {
+			3 : {
+				sorter : 'digit'
+			}
+		}
+	});
+
+	return MateDatabase;
+});
---
+++ app/assets/js/app/views/addMoney.js
@@ -0,0 +1,60 @@
+define([
+// These are path alias that we configured in our bootstrap
+'jquery', // lib/jquery/jquery
+'underscore', // lib/underscore/underscore
+'backbone', 'app/models/junky', 'app/views/junkies/row',
+		'text!templates/addMoneyForm.html' ], function($, _, Backbone, Junky,
+		JunkyView, html) {
+
+	addMoneyFormView = Backbone.View.extend({
+		defaults : {},
+		initialize : function(data) {
+			this.render();
+		},
+		addMoney : function() {
+			var junkies = MateDatabase.junkies;
+			var junkyId = $('select#addMoney-user').val();
+			var junky = MateDatabase.junkies.find(function(item) {
+				return item.get("id") == junkyId;
+			});
+
+			var curAmount = junky.get("credit");
+			var amount = $('input#addMoney-amount').val() * 100;
+
+			junky.save({
+				credit : curAmount + amount
+			}, {
+				success : function(junky) {
+					new JunkyView({
+						el : $('#junky-container'),
+						junky : junky
+					});
+					
+					$("#junkyTable").trigger('update');
+					$("#junkyTable").tablesorter({
+						sortList : [ [ 3, 1 ] ],
+						headers : {
+							3 : {
+								sorter : 'digit'
+							}
+						}
+					});
+				}
+			});
+		},
+		events : {
+			"click button#addMoney-add" : "addMoney",
+		},
+		render : function() {
+			var junkies = MateDatabase.junkies;
+			// Compile the template using underscore
+			var data = {junkies: junkies};
+			var template = _.template(html, data);
+			// Load the compiled HTML into the Backbone "el"
+			this.$el.html(template);
+		}
+	});
+
+	return addMoneyFormView;
+
+});

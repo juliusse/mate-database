09447ca29c4e035e5f82ef6de90ba627332bee31
Julius Seltenheim <mail@julius-seltenheim.com>
2014-12-29 20:19:13 +0100

    updated build files

--- ci-run.sh
+++ ci-run.sh
@@ -17,18 +17,18 @@ dpkg-buildpackage -uc -us -b
 mv ../mate-db_*.deb jenkins/builds/
 
 # move mate-db files
-mv mate-db/target/universal/mate-dabatase-*.zip jenkins/builds/
-mv mate-db/target/scala-2.10/jacoco/jacoco.exec jenkins/jacoco/frontend.exec
+mv application/target/universal/mate-dabatase-*.zip jenkins/builds/
+mv application/target/scala-2.10/jacoco/jacoco.exec jenkins/jacoco/frontend.exec
 
 # remove MVC-classes that should be ignored
-rm -rf mate-db/target/*/classes/system
-rm -rf mate-db/target/*/classes/Routes*.class
-rm -rf mate-db/target/*/classes/info/seltenheim/mate/views
-rm -rf mate-db/target/*/classes/info/seltenheim/mate/controllers/javascript
-rm -rf mate-db/target/*/classes/info/seltenheim/mate/controllers/ref
-rm -rf mate-db/target/*/classes/info/seltenheim/mate/controllers/routes*
-rm -rf mate-db/target/*/classes/info/seltenheim/mate/controllers/Reverse*.class
-rm -rf mate-db/target/*/classes/info/seltenheim/mate/controllers/system/javascript
-rm -rf mate-db/target/*/classes/info/seltenheim/mate/controllers/system/ref
-rm -rf mate-db/target/*/classes/info/seltenheim/mate/controllers/system/routes*
-rm -rf mate-db/target/*/classes/info/seltenheim/mate/controllers/system/Reverse*.class
+rm -rf application/target/*/classes/system
+rm -rf application/target/*/classes/Routes*.class
+rm -rf application/target/*/classes/info/seltenheim/mate/views
+rm -rf application/target/*/classes/info/seltenheim/mate/controllers/javascript
+rm -rf application/target/*/classes/info/seltenheim/mate/controllers/ref
+rm -rf application/target/*/classes/info/seltenheim/mate/controllers/routes*
+rm -rf application/target/*/classes/info/seltenheim/mate/controllers/Reverse*.class
+rm -rf application/target/*/classes/info/seltenheim/mate/controllers/system/javascript
+rm -rf application/target/*/classes/info/seltenheim/mate/controllers/system/ref
+rm -rf application/target/*/classes/info/seltenheim/mate/controllers/system/routes*
+rm -rf application/target/*/classes/info/seltenheim/mate/controllers/system/Reverse*.class
